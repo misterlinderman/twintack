8d86153343a9f1a3e8ec72b46d3cdcbe254879de
Matt Linder <mattlinder@gmail.com>
2024-12-20 00:15:01 -0600

    Restructured the markup to get the sticky header to work. Also rebuilt in preparation for the marquee functionality. Need to rename some ACF components to reflect the marquee terms.

--- themes/twintack2025/front-page.php
+++ themes/twintack2025/front-page.php
@@ -1,10 +1,6 @@
 <?php
 get_header();
 
-// Get header configuration
-get_template_part('template-parts/header/header', 'content');
-
-
 // Get other homepage sections
 get_template_part('template-parts/content', 'flexible');
 
